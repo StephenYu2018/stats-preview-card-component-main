62886b4a973e7fbc5688e4a0465c179820f8a67f
StephenYu2018 <stephenyu8021@gmail.com>
2023-04-14 18:07:21 -0400

    Setup project styling theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 module.exports = {
   content: ['./src/**/*.{html,js}'],
   theme: {
-    extend: {},
+    fontFamily: {
+      inter: ['\'Inter\'', 'sans-serif'],
+      'lexend-deca': ['\'Lexend Deca\'', 'sans-serif'],
+    },
+    extend: {
+      colors: {
+        'very-dark-blue': 'hsl(233, 47%, 7%)',
+        'dark-desaturated-blue': 'hsl(244, 38%, 16%)',
+        'soft-violet': 'hsl(277, 64%, 61%)',
+      },
+    },
   },
   plugins: [],
 }
